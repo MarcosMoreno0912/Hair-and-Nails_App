86465b7e8423a860e4eb0fab3bd6d31051bf7fe6
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-10-30 19:45:58 -0300

    Component Map

--- Client/src/views/Contact/Contact.jsx
+++ Client/src/views/Contact/Contact.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import style from './Contact.module.css'
+import Maps from '../../components/Map/Map.jsx'
+import mapsApi from '../../../.env'
+
+const mapURL= `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${mapsApi.mapsKey}`
 
 const Contact = () => {
 
@@ -7,6 +11,13 @@ const Contact = () => {
 		<>
 			<div className={style.tituloNuestros}>
       	<div className={style.textWrapper}>Contacto</div>
+    	</div>
+    	<div>
+    		<Maps googlemapURL={mapURL}
+    			containerElement={<div style={{height: '400px'}} />}
+    			mapElement={<div style={{height: '100%'}} />}
+    			loadingElement={<p>Cargando</p>}
+    		/>
     	</div>
 		</>
 	)
