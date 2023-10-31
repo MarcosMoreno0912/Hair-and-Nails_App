0c60233f5f035abce2402075305755d16a4e32ef
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-10-31 11:39:03 -0300

    Integración de mapa funcionando

--- Client/src/components/Map/Map.jsx
+++ Client/src/components/Map/Map.jsx
@@ -1,1 +1,24 @@
-Button.jsx
+import React, { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+import style from './Map.module.css'
+
+const Maps = () => {
+	useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY29zbm0yMyIsImEiOiJjbG9lY3Q2azgwZTg3Mmt0OGp2ZXB4dHA1In0.s2-3ffdaukdT-x7S1DudNg'; // Reemplaza con tu clave de acceso
+
+    const map = new mapboxgl.Map({
+      container: 'map', // El ID de un div donde se renderizará el mapa
+      style: 'mapbox://styles/mapbox/streets-v11', // Estilo de mapa (puedes personalizarlo)
+      center: [-33.282766, -66.322073], // Coordenadas del centro del mapa
+      zoom: 16, // Nivel de zoom
+    });
+
+    // Añade marcadores, capas, etc., según tus necesidades.
+
+    return () => map.remove(); // Limpia el mapa al desmontar el componente.
+  }, []);
+
+  return <div id="map" className={style.map} ></div>;
+};
+
+export default Maps;
--- Client/src/views/Contact/Contact.jsx
+++ Client/src/views/Contact/Contact.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import style from './Contact.module.css'
 import Maps from '../../components/Map/Map.jsx'
-import mapsApi from '../../../.env'
-
-const mapURL= `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${mapsApi.mapsKey}`
 
 const Contact = () => {
 
@@ -13,11 +10,7 @@ const Contact = () => {
       	<div className={style.textWrapper}>Contacto</div>
     	</div>
     	<div>
-    		<Maps googlemapURL={mapURL}
-    			containerElement={<div style={{height: '400px'}} />}
-    			mapElement={<div style={{height: '100%'}} />}
-    			loadingElement={<p>Cargando</p>}
-    		/>
+    		<Maps />
     	</div>
 		</>
 	)
