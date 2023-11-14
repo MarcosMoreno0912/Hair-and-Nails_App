655bc85b443274a41f623b449c3d86cc15ae161f
MarcosMoreno0912 <moreno83marcos@gmail.com>
2023-11-14 17:13:14 -0300

    Implement info contact

--- Client/src/views/Contact/Contact.jsx
+++ Client/src/views/Contact/Contact.jsx
@@ -25,6 +25,24 @@ const Contact = () => {
     		<div className={style.form} >
 					<FormContact />
     		</div>
+    		<div className={style.datosContacto}>
+					<div className={style.rectangle} />
+					<div className={style.img} />
+					<div className={style.rectangle2} />
+					<div className={style.rectangle3} />
+					<div className={style.overlap}>
+						<div className={style.vector} />
+						<div className={style.overlapGroup}>
+							<div className={style.textWrapper}>O</div>
+							<p className={style.div}>Puedes contactarte mediante Wsp o Instagram :</p>
+						</div>
+					</div>
+					<div className={style.vector2} />
+					<div className={style.textWrapper2}>@caro.peluqueria_</div>
+					<div className={style.textWrapper3}>@_.mynails</div>
+					<div className={style.textWrapper4}>2664841130 (Carolina)</div>
+					<div className={style.textWrapper5}>2634765094 (Micaela)</div>
+				</div>
     	</div>
 		</>
 	)
